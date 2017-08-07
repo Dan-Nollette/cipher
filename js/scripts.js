d9bd6784aef1e11a8d5256d2468f991701f4e505
Dan Nollette and LDMacKrell <ldmackrell@>
2017-08-07 16:26:46 -0700

    js update for cap, swap functions

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var sentence =(prompt("Enter a preferred sentence:"));
+
+var bookends = function (sentence) {
+  var firstLetter = sentence.charAt(0);
+  var lastLetter = sentence.charAt(sentence.length - 1);
+  var firstLetter = firstLetter.toUpperCase();
+  var lastLetter = lastLetter.toUpperCase();
+  var middleLetters = sentence.substring(1, sentence.length-1);
+  var lettersArray = [firstLetter, middleLetters, lastLetter];
+  return lettersArray;
+};
+
+var capitalize = function(sentence) {
+	var lettersArray = bookends(sentence);
+  return lettersArray[0] + lettersArray[1] + lettersArray[2];
+};
+
+var swap = function(sentence) {
+  var letters = bookends(sentence);
+  return letters[2] + letters[1] + letters[0];
+};
+
+var shorten = function(sentence) {
+  var letters = bookends(sentence);
+  return letters[2] + letters[0];
+}
+
+var combine = function(sentence) {
+  var letters = bookends(sentence);
+  return sentence + shorten(sentence);
+};
+
+// alert(capitalize(sentence));
+//
+// // alert(swap(sentence));
+
+// alert(shorten(sentence));
+
+alert(combine(sentence));
